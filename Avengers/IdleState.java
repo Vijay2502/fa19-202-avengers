a156b9516c26a376baecfeae32b7279ca99b9899
nirmit jasapara <nirmit.jasapara@sjsu.edu>
2019-11-17 22:25:41 -0800

    added Idle State

---
+++ Avengers/IdleState.java
@@ -0,0 +1,27 @@
+/**
+ * Write a description of class IdleState here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class IdleState implements AnimationState
+{
+    public IdleState (Player player)
+    {
+    }
+    public void attack()
+    {
+    }
+    public void flipHorizontally()
+    {
+    }
+    public void superPower()
+    {
+    }
+    public void animationTime()
+    {
+    }
+    public void autoStateChange()
+    {
+    }
+}
