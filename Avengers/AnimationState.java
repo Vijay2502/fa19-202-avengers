152676c93c19ff839a9d84cdfcee7b5a780f0f42
nirmit jasapara <nirmit.jasapara@sjsu.edu>
2019-11-18 12:17:24 -0800

    Implemented all the states

--- Avengers/AnimationState.java
+++ Avengers/AnimationState.java
@@ -4,6 +4,6 @@ public interface AnimationState
     public void attack();
     public void flipHorizontally();
     public void superPower();
-    public void animationTime();
+    public void initAnimation();
     public void autoStateChange();
 }
