81dd425c14d1bc0a19a0ddec06753dd7585b3b2d
Chaitanya Naik <chaitanyauday.naik@sjsu.edu>
2019-11-18 23:39:05 -0800

    Add Elf weapon class

---
+++ Avengers/ElfWeapon.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ElfWeapon here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ElfWeapon extends Enemy
+{
+    
+    public ElfWeapon(Player mainPlayer, Counter counter) {
+        super(mainPlayer, counter);
+        this.health = 2;
+        
+    }
+    
+    public void setScaling() {
+        GreenfootImage image = getImage();
+        image.scale(120, 120);
+    }  
+}
