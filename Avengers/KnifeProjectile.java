af9aac41aa9e66503f89998ac0561a6de1890e93
vijay2502 <vijay.ghanshani@sjsu.edu>
2019-11-20 18:43:38 -0800

    added KnifeProjectile class

---
+++ Avengers/KnifeProjectile.java
@@ -0,0 +1,16 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class KnifeProjectile here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class KnifeProjectile extends EnemyProjectile
+{
+    public KnifeProjectile(String projectileImage, int speed, int damage)
+    {
+        super(projectileImage, speed, damage);
+    }
+     
+}
