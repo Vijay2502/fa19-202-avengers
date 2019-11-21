bdc642c7ea44a84932c9fe3b13819f0de11f01cb
vijay2502 <vijay.ghanshani@sjsu.edu>
2019-11-20 21:11:48 -0800

    added BeamProjectile class

---
+++ Avengers/BeamProjectile.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class BeamProjectile here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BeamProjectile extends HeroProjectile
+{
+    public void act() 
+    {
+        GreenfootImage up = new GreenfootImage("./images/thor_hammer.png");
+        this.speed = 5;
+        this.damage = 20;
+        up.scale(40,40);
+        setImage(up);
+    }   
+}
