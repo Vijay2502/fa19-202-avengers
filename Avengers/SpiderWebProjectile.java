46866e7f2574f3a461c5c243ab9a4f2551c2e83d
vijay2502 <vijay.ghanshani@sjsu.edu>
2019-11-20 21:10:10 -0800

    added SpiderWebProjectile class

---
+++ Avengers/SpiderWebProjectile.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class SpiderWebProjectile here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SpiderWebProjectile extends HeroProjectile
+{
+    public void act() 
+    {
+        GreenfootImage up = new GreenfootImage("./images/thor_hammer.png");
+        this.speed = 5;
+        this.damage = 20;
+        up.scale(40,40);
+        setImage(up);
+    }   
+}
