4db2c4ea7fc8afa210af09752ec1eb2c2357d159
vijay2502 <vijay.ghanshani@sjsu.edu>
2019-11-20 18:41:21 -0800

    added BombProjectile class

---
+++ Avengers/BombProjectile.java
@@ -0,0 +1,16 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class SphereProjectile here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BombProjectile extends EnemyProjectile
+{
+    public BombProjectile(String projectileImage, int speed, int damage)
+    {
+        super(projectileImage, speed, damage);
+    }
+     
+}
