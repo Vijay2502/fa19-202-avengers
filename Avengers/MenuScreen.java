705c451b874e42159950282a0ef366072d54019e
Atharva Munshi <atharvachaitanya.munshi@sjsu.edu>
2019-11-20 19:09:04 -0800

    cap america

--- Avengers/MenuScreen.java
+++ Avengers/MenuScreen.java
@@ -68,7 +68,7 @@ public class MenuScreen extends World
         });
         
         capCommand.setReceiver(() -> {
-            Greenfoot.setWorld(new MyWorld(new capmarvel()));
+            Greenfoot.setWorld(new MyWorld(new capamerica()));
         });
         
         blackWidowCommand.setReceiver(() -> {
--- Avengers/capamerica.java
+++ Avengers/capamerica.java
@@ -1,24 +1,24 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class capmarvel here.
+ * Write a description of class capamerica here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class capmarvel extends Player
+public class capamerica extends Player
 {
     /**
-     * Act - do whatever the capmarvel wants to do. This method is called whenever
+     * Act - do whatever the capamerica wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public capmarvel()
+    public capamerica()
     {
-        GreenfootImage up = new GreenfootImage("./images/cap marvel.png");
+      GreenfootImage up = new GreenfootImage("./images/cap.jpg");
         up.scale(150,150);
         setImage(up);
         this.projSpeed = 10;
-        this.projDamage = 20;        
+        this.projDamage = 10;  
     }
     public void act() 
     {
@@ -27,7 +27,7 @@ public class capmarvel extends Player
     public void displayInfo() {
         String name = this.getClass().getName();
         getWorld().showText(name, 1100, 55);
-        GreenfootImage character = new GreenfootImage("./images/cap marvel.png");
+        GreenfootImage character = new GreenfootImage("./images/cap.jpg");
         GreenfootImage health = new GreenfootImage("./images/health.png");
         GreenfootImage superpower = new GreenfootImage("./images/power.png");
         character.scale(70,70);
