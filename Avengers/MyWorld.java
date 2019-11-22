94e8ce4197843e1ed2c7b695fd5d8ebbf003e957
Kapil Mulchandani <kapilchandralal.mulchandani@sjsu.edu>
2019-11-22 13:36:39 -0800

    Enemies based on levels

--- Avengers/MyWorld.java
+++ Avengers/MyWorld.java
@@ -46,11 +46,11 @@ public class MyWorld extends World
         {
             randomSpawn = Greenfoot.getRandomNumber(5);
             switch (randomSpawn) {
-                case 0: addObject(new Ultron(mainPlayer, counter), getWidth(), 0); break;
+                case 0: addObject(new Elf(mainPlayer, counter), getWidth(), 0); break;
                 case 1: addObject(new Ultron(mainPlayer, counter), getWidth(), getHeight() / 4); break;
-                case 2: addObject(new Loki(mainPlayer, counter), getWidth(), getHeight() / 2); break;
-                case 3: addObject(new GreenGoblin(mainPlayer, counter), getWidth(), 3 *getHeight() / 4); break;
-                case 4: addObject(new Ultron(mainPlayer, counter), getWidth(), getHeight()); break;
+                case 2: addObject(new Elf(mainPlayer, counter), getWidth(), getHeight() / 2); break;
+                case 3: addObject(new Ultron(mainPlayer, counter), getWidth(), 3 *getHeight() / 4); break;
+                case 4: addObject(new Elf(mainPlayer, counter), getWidth(), getHeight()); break;
             }            
         }
     }
