71093d722fddf3b9645b3042e747371564834e0e
Kapil Mulchandani <kapilchandralal.mulchandani@sjsu.edu>
2019-11-21 21:43:38 -0800

    Level 4 added

--- Avengers/Level4.java
+++ Avengers/Level4.java
@@ -1,22 +1,24 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Level3 here.
+ * Write a description of class Level4 here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Level3 extends MyWorld
+public class Level4 extends MyWorld
 {
+
     /**
-     * Constructor for objects of class Level3.
+     * Constructor for objects of class Level4.
      * 
      */
-    public Level3(Player player)
-    {    
+    public Level4(Player player)
+    {
         super(player);
         this.setBackground( new GreenfootImage("./images/SoulStoneLevel.png"));
     }
+    
     public void spawnEnemies () {
         if (count % spawnSpeed == 0)
         {
@@ -34,7 +36,7 @@ public class Level3 extends MyWorld
     public void nextLevelProgressCheck(){
         if(counter.score == 10){
             // Need to change subsequent levels when world class is changed.
-            Greenfoot.setWorld(new Level4(mainPlayer));
+            // Greenfoot.setWorld(new Level3(mainPlayer));
         }
     }
     
