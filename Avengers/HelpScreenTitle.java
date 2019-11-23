751fadd6bb5c2920a62b9081b0569e3968cf986f
Kapil Mulchandani <kapilchandralal.mulchandani@sjsu.edu>
2019-11-22 22:08:02 -0800

    Help Fix

--- Avengers/HelpScreenTitle.java
+++ Avengers/HelpScreenTitle.java
@@ -12,12 +12,11 @@ public class HelpScreenTitle extends Actor
      * Act - do whatever the HelpScreenTitle wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-   
     public void act() 
     {
+        // Add your action code here.
         click();
     }    
-    
     public void click() 
     {
         if(Greenfoot.mouseClicked(this)){
