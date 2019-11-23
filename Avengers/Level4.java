7e42a2bfbaf271fb2f4bfffc1bd2db347c0dec81
Kapil Mulchandani <kapilchandralal.mulchandani@sjsu.edu>
2019-11-22 21:35:29 -0800

    Thanos on Level4

--- Avengers/Level4.java
+++ Avengers/Level4.java
@@ -20,17 +20,7 @@ public class Level4 extends MyWorld
     }
     
     public void spawnEnemies () {
-        if (count % spawnSpeed == 0)
-        {
-            randomSpawn = Greenfoot.getRandomNumber(5);
-            switch (randomSpawn) {
-                case 0: addObject(new Elf(mainPlayer, counter), getWidth(), 0); break;
-                case 1: addObject(new Ultron(mainPlayer, counter), getWidth(), getHeight() / 4); break;
-                case 2: addObject(new Loki(mainPlayer, counter), getWidth(), getHeight() / 2); break;
-                case 3: addObject(new GreenGoblin(mainPlayer, counter), getWidth(), 3 *getHeight() / 4); break;
-                case 4: addObject(new Loki(mainPlayer, counter), getWidth(), getHeight()); break;
-            }            
-        }
+        addObject(new Thanos(mainPlayer, counter), getWidth(), getHeight() / 2);
     }
 
     public void nextLevelProgressCheck(){
