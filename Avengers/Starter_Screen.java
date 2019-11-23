873742350e1c0ae602b19d145d72e51b297499da
Chaitanya Naik <chaitanyauday.naik@sjsu.edu>
2019-11-22 23:30:51 -0800

    stop sound on Starter_Screen , replace images

--- Avengers/Starter_Screen.java
+++ Avengers/Starter_Screen.java
@@ -18,6 +18,7 @@ public class Starter_Screen extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1200, 800, 1);
         SoundManager manager = SoundManager.getInstance();
+        manager.getSound().stop();
         manager.setNewSound("title");
         
         manager.getSound().play();
